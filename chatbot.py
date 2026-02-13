e9c0a34adde2bdd01af973695a166f2a1681fe87
Aleix74 <alessandrodifabio792@gmail.com>
2026-02-13 10:58:46 +0100

    bot telegram AI

--- chatbot.py
+++ chatbot.py
@@ -4,13 +4,19 @@ This program requires the following modules:
 - urllib3==2.6.2
 """
 
+# from turtle import update
+from urllib import response
 from telegram import Update
 from telegram.ext import ApplicationBuilder, MessageHandler, ContextTypes, filters
 import configparser
 import logging
+from ChatGPT_HKBU import ChatGPT
+
+gpt = None
 
 
 def main():
+
     # Configure logging so you can see initialization and error messages
     logging.basicConfig(
         format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
@@ -21,6 +27,9 @@ def main():
     logging.info("INIT: Loading configuration...")
     config = configparser.ConfigParser()
     config.read("config.ini")
+
+    global gpt
+    gpt = ChatGPT(config)
 
     # Create an Application for your bot
     logging.info("INIT: Connecting the Telegram bot...")
@@ -34,13 +43,24 @@ def main():
     logging.info("INIT: Initialization done!")
     app.run_polling()
 
-
-async def callback(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    logging.info("UPDATE: " + str(update))
+    # async def callback(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    #    logging.info("UPDATE: " + str(update))
 
     # send the echo back to the client
-    text = update.message.text.upper()
-    await update.message.reply_text(text)
+    #    text = update.message.text.upper()
+    #    await update.message.reply_text(text)
+
+
+async def callback(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    # await update.message.reply_text(response)
+    logging.info("UPDATE: " + str(update))
+    loading_message = await update.message.reply_text("Thinking...")
+
+    # send the user message to the ChatGPT client
+    response = gpt.submit(update.message.text)
+
+    # send the response to the Telegram box client
+    await loading_message.edit_text(response)
 
 
 if __name__ == "__main__":
